3b34f13081b54ad0d0e93132012ba5e2b8552def
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2023-06-06 11:34:19 +0330

    add user

--- JWTAuth.WebApi/Models/UserInfo.cs
+++ JWTAuth.WebApi/Models/UserInfo.cs
@@ -1,6 +1,8 @@
 ﻿namespace JWTAuth.WebApi.Models
 {
     //https://www.c-sharpcorner.com/article/how-to-implement-jwt-authentication-in-web-api-using-net-6-0-asp-net-core/
+    //pass= $admin@2022
+    //email = admin@abc.com
     public class UserInfo
     {
         public int UserId { get; set; }
