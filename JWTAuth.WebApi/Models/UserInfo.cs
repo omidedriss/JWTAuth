9e112abea09a9c6073913f1681e8b111be5bbfa5
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2023-06-06 11:05:33 +0330

    Add project files.

---
+++ JWTAuth.WebApi/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace JWTAuth.WebApi.Models
+{
+    //https://www.c-sharpcorner.com/article/how-to-implement-jwt-authentication-in-web-api-using-net-6-0-asp-net-core/
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string? DisplayName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
