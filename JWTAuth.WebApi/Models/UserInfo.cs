d1f1579ebf332a6496792e5d0a0c3a612dce6fbb
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2023-06-13 13:16:45 +0330

    add outh2

--- JWTAuth.WebApi/Models/UserInfo.cs
+++ JWTAuth.WebApi/Models/UserInfo.cs
@@ -3,6 +3,11 @@
     //https://www.c-sharpcorner.com/article/how-to-implement-jwt-authentication-in-web-api-using-net-6-0-asp-net-core/
     //pass= $admin@2022
     //email = admin@abc.com
+
+    //oauth
+    //https://github.com/titarenko/OAuth2
+
+    //https://github.com/MrFrank75/ProtectedWebApi
     public class UserInfo
     {
         public int UserId { get; set; }
